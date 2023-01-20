cde8d4a8e3da0b5d303aa5fa40d9caa88e37cea4
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-19 21:18:30 -0500

    s11.1 create page HomaPage

---
+++ semana-11/eakon-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchReadHeroProducts } from "../redux/thunks/homeThunk";
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchReadHeroProducts());
+  }, []);
+
+  return (
+    <h1>HomaPage</h1>
+  );
+};
+
+export default HomePage;
