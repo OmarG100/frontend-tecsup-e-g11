d869cdb8b9840a8f623f0bbf21c4b8d40babd94d
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-19 21:17:34 -0500

    s11.1 create redux homeThunk fetchReadHeroProducts

--- semana-11/eakon-frontend/src/redux/slices/homeSlice.js
+++ semana-11/eakon-frontend/src/redux/slices/homeSlice.js
@@ -1,14 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchReadHeroProducts } from "../thunks/homeThunk";
 
 const initialState = {
   loading: false,
-  error: {}
+  error: {},
+  heroProducts: []
 };
 
 export const homeSlice = createSlice({
   name: 'home',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchReadHeroProducts.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchReadHeroProducts.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.error = {};
+      state.heroProducts = payload;
+    });
+    builder.addCase(fetchReadHeroProducts.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+      state.heroProducts = [];
+    });
+  }
 });
 
 export default homeSlice.reducer;
