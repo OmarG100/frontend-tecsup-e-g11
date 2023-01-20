081dbbf2ba0108fc814840af50447cde90343700
Elliot Garamendi <egaramendi@cineplanet.com.pe>
2023-01-19 19:18:30 -0500

    s11.1 create redux homeSlice

---
+++ semana-11/eakon-frontend/src/redux/slices/homeSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  loading: false,
+  error: {}
+};
+
+export const homeSlice = createSlice({
+  name: 'home',
+  initialState,
+  reducers: {}
+});
+
+export default homeSlice.reducer;
