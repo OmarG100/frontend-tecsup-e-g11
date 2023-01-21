ade52a6c3a74a1270eff341f6026776d65268c93
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-20 20:32:39 -0500

    s11.1 update component HomeHero

--- semana-11/eakon-frontend/src/components/home/HomeHero.jsx
+++ semana-11/eakon-frontend/src/components/home/HomeHero.jsx
@@ -1,5 +1,43 @@
+import { Link } from 'react-router-dom';
+import { A11y, Autoplay, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 const HomeHero = ({ heroProducts }) => {
-  return (<h1>HomeHero</h1>);
+  return (
+    <Swiper
+      tag='section'
+      wrapperTag='div'
+      // className='hero'
+      modules={[A11y, Autoplay, Navigation, Pagination]}
+      loop
+      navigation
+      pagination={{ clickable: true }}
+      preloadImages={false}
+    >
+      {heroProducts.map((element) => {
+        const { nombre, slug, subcategoria, marca, imagen } = element.attributes;
+        return (
+          <SwiperSlide
+            key={element.id}
+            tag='section'
+            zoom={true}
+            className='hero'
+            style={{
+              // backgroundImage: `url(${imagen.data[0].attributes.url})`
+            }}
+          >
+            <div className="container">
+              <div className="card card--hero">
+                <h2 className="card__title card__title--hero">{nombre}</h2>
+                <h3 className="card__subtitle card__subtitle--hero">{marca.data[0].attributes.nombre} I {subcategoria.data.attributes.nombre}</h3>
+                <Link to={`/productos/${slug}`} className="button button--primary">Ver más</Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default HomeHero;
