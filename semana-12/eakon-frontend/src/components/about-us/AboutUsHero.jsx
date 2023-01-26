5925a0339234ab0c3ac9a12873e2c996b77f8091
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-25 21:18:18 -0500

    s12.1 component HeroBanner

--- semana-12/eakon-frontend/src/components/about-us/AboutUsHero.jsx
+++ semana-12/eakon-frontend/src/components/about-us/AboutUsHero.jsx
@@ -1,17 +1,13 @@
+import HeroBanner from "../common/hero/HeroBanner";
+
 const AboutUsHero = ({ aboutUsData }) => {
   const { attributes } = aboutUsData;
   return (
     <>
-      <section
-        className="hero d-flex a-items-center min-h-25vh"
-        style={{
-          backgroundImage: `url(${attributes?.foto.data[0].attributes.formats.large.url})`
-        }}
-      >
-        <div className="container">
-          <h1 className="section__title section__title--lg color-dark-primary">Nosotros</h1>
-        </div>
-      </section>
+      <HeroBanner
+        image={attributes?.foto.data[0].attributes.formats.large.url}
+        title="Nosotros"
+      />
       <section className="section">
         <div className="container d-flex f-direction-column gap-sm">
           <h2 className="section__subtitle">{attributes?.nombre_empresa}</h2>
