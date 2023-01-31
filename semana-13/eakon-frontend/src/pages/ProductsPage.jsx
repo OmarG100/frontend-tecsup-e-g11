e87ad3423059b0af26f77e8f796bcbf6449b55af
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-30 20:15:22 -0500

    s13.1 hook useProducts

--- semana-13/eakon-frontend/src/pages/ProductsPage.jsx
+++ semana-13/eakon-frontend/src/pages/ProductsPage.jsx
@@ -2,22 +2,26 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 // import { useDispatch } from "react-redux";
 import LoadersModal from "../components/common/loaders/LoadersModal";
+import useProducts from "../hooks/useProducts";
 import { fetchReadProducts } from "../redux/thunks/productsThunk";
 // import ContactHero from "../components/contact/ContactHero";
 
 const ProductsPage = () => {
   const dispatch = useDispatch();
   // const { loading, contactData } = useContact();
+  const { loading, products, productsPage, productsPageSize } = useProducts();
 
   useEffect(() => {
     document.title = '¡Productos! I EAKON';
-    console.log('Comida');
-    dispatch(fetchReadProducts());
+    dispatch(fetchReadProducts({ productsPage, productsPageSize }));
   }, []);
 
   return (
     <>
-      {false && <LoadersModal />}
+      {loading && <LoadersModal />}
+      <ul>
+        {products.map(element => <li>{element.attributes.nombre}</li>)}
+      </ul>
       {/* <ContactHero contactData={contactData} /> */}
       {/* <ContactMeans /> */}
     </>
