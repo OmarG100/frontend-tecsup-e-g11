a4e05bea48a1be7a0c61b1a4ffc61b23d10fd6d1
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-30 20:42:51 -0500

    13.1 component ProductsWrapper

--- semana-13/eakon-frontend/src/pages/ProductsPage.jsx
+++ semana-13/eakon-frontend/src/pages/ProductsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 // import { useDispatch } from "react-redux";
-import LoadersModal from "../components/common/loaders/LoadersModal";
+import ProductsWrapper from "../components/products/ProductsWrapper";
 import useProducts from "../hooks/useProducts";
 import { fetchReadProducts } from "../redux/thunks/productsThunk";
 // import ContactHero from "../components/contact/ContactHero";
@@ -17,14 +17,14 @@ const ProductsPage = () => {
   }, []);
 
   return (
-    <>
-      {loading && <LoadersModal />}
+    <ProductsWrapper
+      loading={loading}
+      title="Producto"
+    >
       <ul>
-        {products.map(element => <li>{element.attributes.nombre}</li>)}
+        {products.map(element => <li key={element.id}>{element.attributes.nombre}</li>)}
       </ul>
-      {/* <ContactHero contactData={contactData} /> */}
-      {/* <ContactMeans /> */}
-    </>
+    </ProductsWrapper>
   );
 };
 export default ProductsPage;
