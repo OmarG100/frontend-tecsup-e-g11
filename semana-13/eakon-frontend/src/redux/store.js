0312452f46c80aa70f62e6c93241ccacf298c3c3
elliotgaramendi <elliotgaramendi@gmail.com>
2023-01-30 20:03:11 -0500

    s13.1 redux products

--- semana-13/eakon-frontend/src/redux/store.js
+++ semana-13/eakon-frontend/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import aboutReducer from "./slices/aboutUsSlice";
 import contactReducer from "./slices/contactSlice";
 import homeReducer from "./slices/homeSlice";
+import productsReducer from "./slices/productsSlice";
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
+    products: productsReducer,
     aboutUs: aboutReducer,
     contact: contactReducer
   }
