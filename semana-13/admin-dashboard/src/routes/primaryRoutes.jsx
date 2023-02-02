3442eb0e7858d7a7a230520fdacd8e6accc445f8
elliotgaramendi <elliotgaramendi@gmail.com>
2023-02-01 20:01:34 -0500

    navegación y layout principal

--- semana-13/admin-dashboard/src/routes/primaryRoutes.jsx
+++ semana-13/admin-dashboard/src/routes/primaryRoutes.jsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import PrimaryLayout from "../layouts/PrimaryLayout";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
 
 export const primaryRoutes = createBrowserRouter([
   {
     path: '/',
+    element: <PrimaryLayout />,
     children: [
       {
         index: true,
-        element: <h1>Home</h1>,
+        element: <HomePage />
       },
       {
         path: 'login',
-        element: <h1>Login</h1>
+        element: <LoginPage />
       }
     ]
   }
