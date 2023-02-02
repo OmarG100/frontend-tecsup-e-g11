0afddef39b4e52038ea2bc9f79fb8141f2150fa4
elliotgaramendi <elliotgaramendi@gmail.com>
2023-02-01 20:27:58 -0500

    UserContext inicial

--- semana-13/admin-dashboard/src/pages/LoginPage.jsx
+++ semana-13/admin-dashboard/src/pages/LoginPage.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
 const LoginPage = () => {
-  return (<h1>LoginPage</h1>);
+  const { user } = useContext(UserContext);
+  return (<h1>LoginPage: {user ? 'Online' : 'Offline'}</h1>);
 };
 
 export default LoginPage;
