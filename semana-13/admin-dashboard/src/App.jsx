6c7fe4485c56e3b14910cd5c95f3f2a1d353562a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-02-02 22:17:38 -0500

    s13.2 register

--- semana-13/admin-dashboard/src/App.jsx
+++ semana-13/admin-dashboard/src/App.jsx
@@ -1,13 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import { AuthProvider } from './context/AuthContext';
 import { primaryRoutes } from './routes/PrimaryRoutes';
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={primaryRoutes} />
-    </AuthProvider>
+    <RouterProvider router={primaryRoutes} />
   );
 }
 
