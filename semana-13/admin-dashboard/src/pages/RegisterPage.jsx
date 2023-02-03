4b02fa6853b694eb20eec93a9beacf9b44a4da09
elliotgaramendi <elliotgaramendi@gmail.com>
2023-02-02 19:46:39 -0500

    s13.2 update RegisterPage form

--- semana-13/admin-dashboard/src/pages/RegisterPage.jsx
+++ semana-13/admin-dashboard/src/pages/RegisterPage.jsx
@@ -1,6 +1,52 @@
+import { useState } from "react";
+
 const RegisterPage = () => {
+
+  const [registrationForm, setRegistrationForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = registrationForm;
+
+  const handleChangeRegistrationForm = (e) => {
+    setRegistrationForm({
+      ...registrationForm,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmitCreateUser = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <h1>RegisterPage</h1>
+    <>
+      <h1>RegisterPage</h1>
+      <form
+        onSubmit={handleSubmitCreateUser}
+      >
+        <input
+          type="email"
+          name="email"
+          placeholder="Correo"
+          value={email}
+          onChange={handleChangeRegistrationForm}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={handleChangeRegistrationForm}
+        />
+        <br />
+        <br />
+        <input type="submit" value="Registrar" />
+      </form>
+    </>
   );
 };
 
