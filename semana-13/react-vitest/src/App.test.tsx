5e6f502a480de6a2a20bba909293bff61395d14b
elliotgaramendi <elliotgaramendi@gmail.com>
2023-02-03 22:21:54 -0500

    s13.3 component Accordion

--- semana-13/react-vitest/src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from "vitest";
-
-describe('App', () => {
-  test('Should multiply two number', () => {
-    expect(3 * 3).toBe(9);
-  });
-});
--- semana-13/react-vitest/src/App.tsx
+++ semana-13/react-vitest/src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import Accordion from './components/Accordion';
 
 function App() {
   return (
-    <h1>React Vitest</h1>
+    <>
+      <h1>React Vitest</h1>
+      <Accordion title="JSX">
+        <p>Extensión de la sintaxis de JavaScript. Recomendamos usarlo con React para describir cómo debería ser la interfaz de usuario.</p>
+      </Accordion>
+    </>
   )
 }
 
