6dc2f1d7bf82a073e48d8eda9f993c63d5b9fb13
elliotgaramendi <elliotgaramendi@gmail.com>
2023-02-03 22:59:32 -0500

    s13.3 content not display

--- semana-13/react-vitest/src/components/Accordion.test.tsx
+++ semana-13/react-vitest/src/components/Accordion.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Accordion from "./Accordion";
 
 describe('Accordion', () => {
-  test('render title', () => {
+  beforeEach(() => {
     render(
       <Accordion
         title="G11"
@@ -10,6 +10,13 @@ describe('Accordion', () => {
         <p>Este es mi super test</p>
       </Accordion>
     );
+  });
+
+  test('render title', () => {
     expect(screen.getByText('G11')).toBeDefined();
+  });
+
+  test('content not display', () => {
+    expect(screen.queryByText(/test/i)).toBeNull();
   });
 });
